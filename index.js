57ea8e36197b9e0532e784d36c70371032116ad5
Finn Pauls <mbp@pauls.dev>
2022-05-28 19:15:28 +0200

    Change the resolution, so pixel art is readable in all angles

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-import * as PIXI from "pixi.js"
-import WebFont from "webfontloader"
-import parseTextures from './lib/parse-textures'
+import * as PIXI from 'pixi.js'
+import WebFont from 'webfontloader'
+import Sprite from './components/Sprite'
 import Entity from './entities/Entity'
 import Game from './lib/Game'
-import Sprite from "./components/Sprite"
+import parseTextures from './lib/parse-textures'
 
 const SPRITESHEET = 'spritesheet.json'
 
@@ -11,50 +11,51 @@ const width = 166
 const height = 144
 
 function start() {
-    const app = createApp()
-    app.loader.add(SPRITESHEET).load(setup)
+  const app = createApp()
+  app.loader.add(SPRITESHEET).load(setup)
 
-    function setup(loader, resources) {
-        const textures = parseTextures(resources[SPRITESHEET].textures)
-        Sprite.textures = textures
+  function setup(loader, resources) {
+    const textures = parseTextures(resources[SPRITESHEET].textures)
+    Sprite.textures = textures
 
-        app.stage.sortableChildren = true
+    app.stage.sortableChildren = true
 
-        app.ticker.add(gameLoop)
+    app.ticker.add(gameLoop)
 
-        window.stage = app.stage
+    window.stage = app.stage
 
-        Entity.init(app.stage)
-        Entity.game = game
+    Entity.init(app.stage)
+    Entity.game = game
 
-        const game = new Game({ stage: app.stage, width, height })
+    const game = new Game({ stage: app.stage, width, height })
 
-        function gameLoop(dt) {
-            Entity.updateAll(dt)
-            game.update(dt)
-        }
+    function gameLoop(dt) {
+      Entity.updateAll(dt)
+      game.update(dt)
     }
+  }
 }
 
 function createApp() {
-    const app = new PIXI.Application({
-        width,
-        height,
-        backgroundColor: 0x000000,
-        antialias: false,
-    })
+  const app = new PIXI.Application({
+    width,
+    height,
+    backgroundColor: 0x000000,
+    antialias: true,
+    resolution: 6,
+  })
 
-    document.body.appendChild(app.view)
+  document.body.appendChild(app.view)
 
-    PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
+  PIXI.settings.SCALE_MODE = PIXI.SCALE_MODES.NEAREST
 
-    return app
+  return app
 }
 
 WebFont.load({
-    custom: {
-        families: ["Silkscreen"],
-        urls: ["assets/fonts/fonts.css"],
-    },
-    active: start,
+  custom: {
+    families: ['Silkscreen'],
+    urls: ['assets/fonts/fonts.css'],
+  },
+  active: start,
 })
